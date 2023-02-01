db4d6b023cc8886b765fec2b4f15e298732df334
Siri <124091206+SiriFromPhotop@users.noreply.github.com>
2023-02-01 10:00:35 -0500

    Create send.js

---
+++ howto/send.js
@@ -0,0 +1,10 @@
+import { Client, localConfig, colors } from 'chatapp.js'
+
+let bot = new Client({
+  name: "BotName",
+  websocket: "wss://chatappbackend.sirifromphotop.repl.co (or any websocket server)",
+  apikey: "ApiKey",
+  color: 1 //choose a number 1 - 13 for the color
+});
+
+bot.leave() // sends a leave message, then ends all execution
