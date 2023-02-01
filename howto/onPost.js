d9693414a12efcb7b4b40f258a5a9d01e5f5b2cd
Siri <124091206+SiriFromPhotop@users.noreply.github.com>
2023-02-01 09:59:58 -0500

    Update onPost.js

--- howto/onPost.js
+++ howto/onPost.js
@@ -1,7 +1,15 @@
-bot.onReady(function() { // go to onReady.js to know how to use this
-  bot.onPost(function(msg) {
-    if (msg == "module_test") {
-      bot.send("Success!");
-    }
-  })
+import { Client, localConfig, colors } from 'chatapp.js'
+
+let bot = new Client({
+  name: "BotName",
+  websocket: "wss://chatappbackend.sirifromphotop.repl.co (or any websocket server)",
+  apikey: "ApiKey",
+  color: 1 //choose a number 1 - 13 for the color
+});
+
+bot.onPost(function(msg) {
+  /*put code here
+  if (msg == "/test-command") {
+    //put code to execute when that text is sent here
+  }*/
 })
