2628533165156f83f03c2baec2641bb861e3c358
Siri <124091206+SiriFromPhotop@users.noreply.github.com>
2023-02-01 09:57:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ export let localConfig = {
   }
 }
 
+export let colors = ["white","red","orange","yellow","green","blue","purple","magenta","silver","blueviolet","chartreuse","cyan","fuchsia"]
+
 export class Client {
   constructor(jsonreal) {
     this.name = jsonreal.name;
     this.ws = new WebSocket(jsonreal.websocket);
-    this.apikey = jsonreal.apikey
+    this.apikey = jsonreal.apikey;
+    if (colors[jsonreal.color] == undefined) {
+      this.color = 1
+    } else {
+      this.color = colors[jsonreal.color - 1]
+    }
   }
   
   send(string) {
@@ -26,7 +33,8 @@ export class Client {
         user: this.name,
         content: string,
         isNewUserAlert: false,
-        ip: this.apikey
+        ip: this.apikey,
+        color: this.color
       }))
       localConfig.addToRate();
     }
@@ -42,12 +50,28 @@ export class Client {
   }
   onReady(callback) {
     if (typeof callback == "function") {
+      console.log("Establised Connection with Chatapp!")
       this.ws.on('open', () => {
         this.ws.send(JSON.stringify({
           user: this.name,
           isNewUserAlert: true,
           ip: this.apikey
         }))
+        callback()
+      })
+    }
+  }
+  leave() {
+    this.ws.send(JSON.stringify({
+      user: this.name,
+      isLeaveMessage: true
+    }))
+    throw new Error('Connection with Chatapp has ended.');
+  }
+  onEnd(callback) {
+    if (typeof callback == "function") {
+      this.ws.on('close', () => {
+        console.log("Lost connection with Chatapp")
         callback()
       })
     }
