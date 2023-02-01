e21a77bf74e41be8aef3fe1f46a1438e8210ed19
Siri <124091206+SiriFromPhotop@users.noreply.github.com>
2023-02-01 10:01:20 -0500

    Create send.js

---
+++ howto/send.js
@@ -0,0 +1,12 @@
+import { Client, localConfig, colors } from './chatapp.js'
+
+let bot = new Client({
+  name: "BotName",
+  websocket: "wss://chatappbackend.sirifromphotop.repl.co (or any websocket server)",
+  apikey: "ApiKey",
+  color: 1 //choose a number 1 - 13 for the color
+});
+
+bot.onReady(function() {
+  //put code here
+})
