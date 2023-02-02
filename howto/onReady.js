060b2304120a9f7f60389bf1053d68048498beef
Siri <124091206+SiriFromPhotop@users.noreply.github.com>
2023-02-02 13:47:15 -0500

    Update onReady.js

--- howto/onReady.js
+++ howto/onReady.js
@@ -4,7 +4,7 @@ let bot = new Client({
   name: "BotName",
   websocket: "wss://chatappbackend.sirifromphotop.repl.co (or any websocket server)",
   apikey: "ApiKey",
-  color: 1 //choose a number 1 - 13 for the color
+  color: 1 //choose a number 1 - 32 for the color
 });
 
 bot.onReady(function() {
